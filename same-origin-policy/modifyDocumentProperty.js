966dda36e2d5312b2284a422997097b714ab1251
wangmeng <kami518@126.com>
2017-10-21 08:28:39 +0800

    what is same origin policy

---
+++ same-origin-policy/modifyDocumentProperty.js
@@ -0,0 +1,6 @@
+/**
+ * Created by wangmeng on 17-10-19.
+ */
+var writeASign = document.createElement("p");
+writeASign.textContent = "我可以修改";
+document.getElementById("a").appendChild(writeASign);
